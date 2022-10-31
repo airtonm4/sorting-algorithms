77968b6db866893fa90e25710850133415deb39a
airtonm4 <airton.atmi@gmail.com>
2022-10-30 22:44:26 -0300

    Added QuickSort

--- src/SortAlgorithms.java
+++ src/SortAlgorithms.java
@@ -2,11 +2,11 @@ import java.util.Arrays;
 
 public class SortAlgorithms {
 
-    public static void InsetionSort(Integer[] array) {
-        Integer j = 0;
+    public static void InsetionSort(int[] array) {
+        int j = 0;
 
         for (int i = 0; i < array.length; i++) {
-            Integer aux = array[i];
+            int aux = array[i];
             j = i;
 
             while (j > 0 && aux <= array[j - 1]) {
@@ -19,14 +19,14 @@ public class SortAlgorithms {
         System.out.println(Arrays.toString(array));
     }
 
-    public static void ShellSort(Integer[] array) {
-        Integer n = array.length;
+    public static void ShellSort(int[] array) {
+        int n = array.length;
 
         for (int gap = n / 2; gap > 0; gap /= 2) {
             System.out.println(gap);
-            for (Integer i = gap; i < n; i++) {
-                Integer key = array[i];
-                Integer j = i;
+            for (int i = gap; i < n; i++) {
+                int key = array[i];
+                int j = i;
 
                 while (j >= gap && array[j - gap] > key) {
                     array[j] = array[j - gap];
@@ -41,8 +41,54 @@ public class SortAlgorithms {
         System.out.println(Arrays.toString(array));
     }
 
-    public static void QuickSort() {
+    // QuickSort
+
+    public static void QuickSort(int[] array) {
+        sort(array, 0, array.length - 1);
+        System.out.println(Arrays.toString(array) + "\n");
+
+        for (int i = 0; i < 50; i++) {
+            System.out.print(array[i] + ", ");
+        }
     }
+
+    private static void sort(int[] array, int init, int end) {
+        if (init < end) {
+            int pivot = partition(array, init, end);
+            // System.out.println(Arrays.toString(array));
+            sort(array, init, pivot - 1);
+            sort(array, pivot + 1, end);
+        }
+    }
+
+    private static int partition(int[] array, int init, int end) {
+        int pivot = array[init];
+        int i = init + 1;
+        int j = end;
+
+        while (i <= j) {
+            if (array[i] <= pivot) {
+                i++;
+            } else if (pivot < array[j]) {
+                j--;
+            } else {
+                int change = array[i];
+
+                array[i] = array[j];
+                array[j] = change;
+
+                i++;
+                j--;
+            }
+
+        }
+
+        array[init] = array[j];
+        array[j] = pivot;
+        return j;
+    }
+
+    // HeapSort
 
     public static void HeapSort() {
     }
