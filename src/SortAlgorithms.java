e07ed2321aa65b7c6fc7641dd9115134c26d955a
airtonm4 <airton.atmi@gmail.com>
2022-10-30 21:21:20 -0300

    Added ShellSort

--- src/SortAlgorithms.java
+++ src/SortAlgorithms.java
@@ -19,4 +19,31 @@ public class SortAlgorithms {
         System.out.println(Arrays.toString(array));
     }
 
+    public static void ShellSort(Integer[] array) {
+        Integer n = array.length;
+
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            System.out.println(gap);
+            for (Integer i = gap; i < n; i++) {
+                Integer key = array[i];
+                Integer j = i;
+
+                while (j >= gap && array[j - gap] > key) {
+                    array[j] = array[j - gap];
+                    j -= gap;
+                }
+
+                array[j] = key;
+                System.out.println(Arrays.toString(array));
+            }
+        }
+
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void QuickSort() {
+    }
+
+    public static void HeapSort() {
+    }
 }
