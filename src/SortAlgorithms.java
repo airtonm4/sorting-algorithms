5275e31966d2cbba5fdf833cc25dc5cd4585261b
airtonm4 <airton.atmi@gmail.com>
2022-10-30 21:20:31 -0300

    Added insertionSort

--- src/SortAlgorithms.java
+++ src/SortAlgorithms.java
@@ -1,3 +1,22 @@
+import java.util.Arrays;
+
 public class SortAlgorithms {
+
+    public static void InsetionSort(Integer[] array) {
+        Integer j = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            Integer aux = array[i];
+            j = i;
+
+            while (j > 0 && aux <= array[j - 1]) {
+                array[j] = array[j - 1];
+                j--;
+            }
+            array[j] = aux;
+        }
+
+        System.out.println(Arrays.toString(array));
+    }
 
 }
